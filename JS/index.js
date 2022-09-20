6eb52e8919182b25a677b496a06b4adb97c8452c
B5th <adado2017@gmail.com>
2022-09-20 13:23:20 -0700

    firstjavapractice

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById("ladies").innerHTML = 'this is a paragraph'
+document.getElementById("ladies").style.color = 'brown'
+
+let firstname = 10
+var surname = 20
+
+const both = firstname + surname
+
+alert(both)
+  both= 50
+alert(both)
+
+function showalertfunction () {
+    
+}
