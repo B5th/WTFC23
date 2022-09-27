1db52105e0a52a989bb1e18f4faffe4b0de45cff
B5th <adado2017@gmail.com>
2022-09-27 12:18:26 -0700

    script statement

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+//const eggprice= 100;
+
+//switch (eggprice) {
+   // case 20: console.log ("i am 20");
+   // break;
+   // case 45: console.log ("i am 45");
+   // break;
+    //case 70: console.log ("i am 70");
+    //break;
+    //case 80: console.log ("i am 80");
+    //break;
+    //default: console.log("none of the above")
+//}
+
+
+const breadprice = 100;
+
+switch (breadprice ) {
+    case 10:
+    case 20:
+    case 30: console.log ("i want bread")
+    break;
+    default: console.log ("none of the above")
+}
