6c19408097bff3c6dfe0c1134f84e5ee4e508c63
B5th <adado2017@gmail.com>
2022-09-29 12:33:33 -0700

    Loop Lesson-For

---
+++ JS/loop.js
@@ -0,0 +1,12 @@
+  for (let num = 1000; num >=1; num = num - 1) {
+    console.log (num)
+  }
+  //write a js code to print numbers from 1000-1
+
+
+                         //OR 
+
+   for (let num = 1000; num >=1; num = num - 1) {
+     console.log (num)
+  }                      
+                           
