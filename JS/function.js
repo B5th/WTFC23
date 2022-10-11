d2498c1330f67f17a393fcb02271bd32c2b2b815
B5th <adado2017@gmail.com>
2022-10-11 14:58:25 -0700

    Function

---
+++ JS/function.js
@@ -0,0 +1,17 @@
+function classlist() {
+    console.log("75 people")
+}
+classlist()
+
+
+function addby2 (num, num2, num3) {
+      console.log(num +2, num2 * 3, num3 +2);
+}
+addby2(2,3,4)
+
+function multiplyby3(num,num2,num3) {
+    const result = "num " + "num2 " + "num";
+    return result;
+}
+const value = multiplyby3(50,20,30)
+console.log(value)
