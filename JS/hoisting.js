37f595efa418e61b76b9ef49cff10614b3a6fead
B5th <adado2017@gmail.com>
2022-10-18 14:27:02 -0700

    hoisting - js interpreter

---
+++ JS/hoisting.js
@@ -0,0 +1,10 @@
+hot ();
+
+function hot() {
+    console.log("this is dope")
+}
+
+console.log(age);
+
+/*var age = 19;   console would show undefined*/
+let age = 19; /* console would ask us to initialize before access  */
